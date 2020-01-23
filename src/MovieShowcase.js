af9fd8d683faacb110cdcc4ce3f095e7b9bfcd2c
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-23 12:38:37 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    movieData.map(data => <MovieCard />);
+  };
+
+  render() {
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
+}
+
 
   render() {
     return (
